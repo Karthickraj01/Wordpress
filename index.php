492f1ed2f296601fb79f68da6c136b50d0fe600c
Karthickraj01 <karthickraj.muniappan@gmail.com>
2024-07-15 17:06:06 +0530

    just try

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Addition</title>
+</head>
+
+<body>
+
+
+    <form action="php" method="post">
+        Table: <input type="text" name="name" id="table"><br>
+        Rows: <input type="text" name="email" id="Rows"><br>
+        <input type="submit">
+        <?php
+echo $_POST['table'];
+?>
+    </form>
+</body>
+
+</html>
